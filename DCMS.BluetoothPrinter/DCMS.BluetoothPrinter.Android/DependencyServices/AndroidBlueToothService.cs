7c909c10f32ab4f4eec0249d14f4570a0f2dee39
zhenghong.chen@racingandsports.com.au <zhenghong.chen@racingandsports.com.au>
2020-03-03 12:45:23 +0800

    no message

---
+++ DCMS.BluetoothPrinter/DCMS.BluetoothPrinter.Android/DependencyServices/AndroidBlueToothService.cs
@@ -0,0 +1,60 @@
+﻿using Android.Bluetooth;
+using DCMS.BlueToothPrinter.DependencyServices;
+using DCMS.BlueToothPrinter.Droid.DependencyServices;
+using Java.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[assembly: Xamarin.Forms.Dependency(typeof(AndroidBlueToothService))]
+namespace DCMS.BlueToothPrinter.Droid.DependencyServices
+{
+    class AndroidBlueToothService : IBlueToothService
+    {
+        /// <summary>
+        /// 获取本地设备蓝牙适配器列表
+        /// </summary>
+        /// <returns></returns>
+        public IList<string> GetDeviceList()
+        {
+            using (BluetoothAdapter bluetoothAdapter = BluetoothAdapter.DefaultAdapter)
+            {
+                var btdevice = bluetoothAdapter?.BondedDevices.Select(i => i.Name).ToList();
+                return btdevice;
+            }
+        }
+
+        /// <summary>
+        /// 使用UUID找到具有选定设备名称的蓝牙设备
+        /// </summary>
+        /// <param name="deviceName">选定设备名称</param>
+        /// <param name="text">消息</param>
+        /// <returns></returns>
+        public async Task Print(string deviceName, string text)
+        {
+            using (BluetoothAdapter bluetoothAdapter = BluetoothAdapter.DefaultAdapter)
+            {
+                BluetoothDevice device = (from bd in bluetoothAdapter?.BondedDevices
+                                          where bd?.Name == deviceName
+                                          select bd).FirstOrDefault();
+                try
+                {
+                    using (BluetoothSocket bluetoothSocket = device?.
+                        CreateRfcommSocketToServiceRecord(
+                        UUID.FromString("00001101-0000-1000-8000-00805f9b34fb")))
+                    {
+                        bluetoothSocket?.Connect();
+                        byte[] buffer = Encoding.UTF8.GetBytes(text);
+                        bluetoothSocket?.OutputStream.Write(buffer, 0, buffer.Length);
+                        bluetoothSocket.Close();
+                    }
+                }
+                catch (Exception exp)
+                {
+                    throw exp;
+                }
+            }
+        }
+    }
+}
---
+++ DCMS.BluetoothPrinter/DCMS.BluetoothPrinter/DependencyServices/IBlueToothService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DCMS.BlueToothPrinter.DependencyServices
+{
+    public interface IBlueToothService
+    {
+        IList<string> GetDeviceList();
+        Task Print(string deviceName, string text);
+    }
+}
